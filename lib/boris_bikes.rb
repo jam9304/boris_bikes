e3a764557a68bf2f70d34ae95c682294d8567a54
Nicola Cortese <nicolacortese@Nicolas-MacBook-Air.local>
2022-02-22 16:38:08 +0000

    Added unit test for docking a bike at a docking station

--- lib/boris_bikes.rb
+++ lib/boris_bikes.rb
@@ -1,9 +1,14 @@
 class DockingStation
     def release_bike
+        bike = Bike.new
+    end
+    def dock_bike(bike)
+        true
     end
 end
 
 class Bike
     def working?
+        true
     end
 end
--- spec/docking_spec.rb
+++ spec/docking_spec.rb
@@ -9,7 +9,11 @@ describe DockingStation do
    expect(subject.release_bike).to be_an_instance_of(Bike)
  end
 
-it 'is the bike working?' do
-  expect(subject.release_bike.working?).to eq true
-end
+  it 'is the bike working?' do
+    expect(subject.release_bike.working?).to eq true
+  end
+  it 'Can I dock the bike?' do
+    bike = Bike.new
+    expect(subject.dock_bike(bike)).to eq true
+  end
 end
